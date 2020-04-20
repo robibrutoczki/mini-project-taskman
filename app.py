a4743e1d567a20f6b9e7fd2156c0cf40cadad784
robibrutoczki <robibrutoczki@googlemail.com>
2020-04-20 16:29:22 +0000

    ready to deploy

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-pncmp.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://password@myfirstcluster-pncmp.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
